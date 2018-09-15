4bc47c4bd2b5b81594ebdeecb0bcb9cacadd2466
Alexander Alan F. Cahutay <xandercahutay1994@gmail.com>
2018-09-15 19:58:02 +0800

    added echo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,7 +15,7 @@
 			                    <li class="archive-item">
 			                        <article class="card">
 			                            <a href="{{ url('/single/' . $value->id) }}" class="card-link">
-			                                <img src="{{ asset($value->image)" alt="" class="card-image">
+			                                <img src="{{ asset($value->image) }}" alt="" class="card-image">
 			                                <div class="card-bottom">
 			                                    <h1 class="card-title">{{ $value->title }}</h1>
 			                                    <time class="card-date" datetime="{{$value->posted_at}}">
