96070f1571b5df2cfbc326c42bca99932703dbce
Alexander Alan F. Cahutay <34784441+xandercahutay1994@users.noreply.github.com>
2018-09-16 23:26:32 +0800

    Article Image

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,7 +15,7 @@
 			                    <li class="archive-item">
 			                        <article class="card">
 			                            <a href="{{ url('/single/' . $value->id) }}" class="card-link">
-			                                <img src="{{ asset('/articleImages/' . $value->image) }}" alt="" class="card-image">
+			                                <img src="{{ asset('/articleImages/' . $value->image) }}" alt="Article Image" class="card-image">
 			                                <div class="card-bottom">
 			                                    <h1 class="card-title">{{ strToUpper($value->title) }}</h1>
 			                                    <time class="card-date" datetime="{{$value->created_at}}">
